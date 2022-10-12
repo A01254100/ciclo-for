78243e26c8db449d21eac30227755341b5023325
A01254100 <112582780+A01254100@users.noreply.github.com>
2022-10-12 12:26:24 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+basic.forever(function () {
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(0, Y)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(1, Y)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(2, Y)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(3, Y)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(4, Y)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+    basic.showIcon(IconNames.Yes)
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(0, Y)
+        basic.pause(120)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(1, Y)
+        basic.pause(120)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(2, Y)
+        basic.pause(120)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(3, Y)
+        basic.pause(120)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(4, Y)
+        basic.pause(120)
+        basic.clearScreen()
+    }
+    basic.showIcon(IconNames.Yes)
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(0, Y)
+        basic.pause(40)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(1, Y)
+        basic.pause(40)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(2, Y)
+        basic.pause(40)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(3, Y)
+        basic.pause(40)
+        basic.clearScreen()
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        led.plot(4, Y)
+        basic.pause(40)
+        basic.clearScreen()
+    }
+    basic.showIcon(IconNames.Yes)
+})
